6f9804fb7ad618bc7c375076c600bf2107f7149c
Prim <tombo201@gmail.com>
2024-12-07 20:03:33 -0500

    Created main.sh (main executable). gen_year_files.sh works now. Created gitignore.

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# main.sh
+# This file is the entry point of the program. It is used to execute the scripts in /scripts/ sequentially.
+
+# Modify as needed
+DIR=$(cd "$(dirname "$0")" && pwd) #get current directory
+
+# Execute scripts sequentially
+${DIR}/scripts/gen_year_files.sh $DIR/transcripts
+
+
--- scripts/gen_year_files.sh
+++ scripts/gen_year_files.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 # gen_year_files.sh
-# This script takes a raw CSV file containing all of the State of the Union addresses, and organizes them into a directory. Each address will be saved in a separate file, named by the year of the address, and populated with its respective transcript.
-# The intent is to make the process of working on the data cleaner and more streamlined.
+# This script receives the path to the transcripts directory as an argument.
+# This script will utilize a raw CSV file containing all of the State of the Union addresses, and organizes them into a directory. Each address will be saved in a separate file, named by the year of the address, and populated with its respective transcript. The intent is to make the process of working on the data cleaner and more streamlined.
 
-DIR="$HOME/project/transcripts" # working directory
-CSV_fp="$DIR/SOTUT.csv"
-YEARS_dir="$DIR/years"
+# Modify as needed
+STOP=0 # if 1, generates only the first file - for testing purposes
 regex="^(.+),(.+),.*,\"\[(.*)\]\""
-STOP=0
 
-if [ -f $CSV_fp ]; then
-	echo "Transcript CSV file found."
-else
-	echo "Transcript CSV file not found."
-	echo "Attempted directory: ${CSV_fp}"
+
+# Ensuring we have the files to work with
+if [ ! -d "$1" ]; then
+	echo "[$(basename "$0")]: Fatal error: Did not receive a working directory as an argument."
+	exit 1
 fi
 
+if [ ! -f "$1/SOTUT.csv" ]; then
+	echo "[$(basename "$0")]: Fatal error: Transcript CSV file not found."
+	exit 1
+fi
 
+DIR_transcripts="$1"
+FP_csv="${DIR_transcripts}/SOTUT.csv"
+first_year=0
+curr_year=0
+
+echo "[$(basename "$0")]: Working..."
+# Each year's transcript is printed on a distinct line. We will first ensure that the line conforms to the expected regex.
 while IFS= read -r address; do
-	#echo "here: $address"
 	if [[ ${address} =~ ${regex} ]]; then
+		# If we reach here, then the regex is working on the current line. Let's divide each column. The regular expression should have three capture groups.
 		president="${BASH_REMATCH[1]}"
 		year="${BASH_REMATCH[2]}"
 		speech="${BASH_REMATCH[3]}"
-		echo "year: ${year}"
-		echo "president: ${president}"
-		echo "speech: ${speech:0:50}"
+		curr_year=$year #for end reporting
+		[ $first_year -eq 0 ] && first_year=$year #for end reporting	
+		
+		#The file will be named with the year, and the contents will have the name of the president, followed by a new line with the raw transcript.
+		echo -e "<<$president>>\n${speech}" > "$DIR_transcripts/years/${year}_raw.txt"
 	else
-		echo "Line ${address} had unexpected format. Terminating."
+		echo "[$(basename "$0")]: Fatal error: Address beginning with \"${address:0:100}...\" had an unexpected format."
 		exit 1
 	fi
 	if [[ STOP -eq 1 ]]; then
-		exit 0
+		break
 	fi
-done < <(tail -n +2 "$CSV_fp")
+done < <(tail -n +2 $FP_csv)
+
+echo "[$(basename "$0")]: Successfully generated raw transcript files for years ${first_year} through ${curr_year}."
 
