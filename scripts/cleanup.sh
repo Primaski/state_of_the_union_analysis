5c2b361e48efc6d924815247d78a96bbb03e11f1
Prim <tombo201@gmail.com>
2024-12-10 02:45:57 -0500

    Fixed up cleanup.sh entirely, preprocessing stage should be finished. Replaced data folder with findings, created resources folder and temp folder.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,16 +2,10 @@
 #! cleanup.sh
 #! The purpose of this script is to preprocess the raw data. It will require the transcripts/years/ directory to be populated.
 
-# Modify below as needed
-#chars_to_remove="\"#%&'()[]*+,:;./=?-_\`|~°¸‑–—‘’“”…$£бЅјў�"
-#stop_chars="\"#%&'()[]*+,:;./=?-_\`|~°–—‘’“”…$£"
-
-# I used `egrep -o "[^A-Za-z0-9]' SOTUT.csv | sort | uniq` to determine all non alphabetic and numerical characters used in the speech.
+# Modify as needed
 STOP=1 # for testing purposes, only does it on the first file. 
 
-
 # Do not modify beyond here
-
 
 # Ensuring we have the files to work with
 if [ ! -d "${1}/years" ]; then
@@ -24,6 +18,8 @@ fi
 DIR_transcripts=$1
 DIR_years="${DIR_transcripts}/years"
 year_files=(${DIR_years}/*.txt)
+first_year=0
+curr_year=0
 
 if [ "${year_files[0]}" == "${DIR_years}/*.txt" ]; then
 	# If equal, there was no glob expansion (no text files), and the string literal is preserved.
@@ -33,17 +29,24 @@ fi
 
 for file in $year_files; do
 	if [[ ! "$file" =~ [0-9]{4}\.txt ]]; then
-	       continue 
-       fi	       
-	echo "Working on file $(basename "$file")"
-	#sed "s/[$stop_chars]//g" "$file" > "$DIR_years/$(basename "$file" .txt)_clean.txt"
-	sed 's/[^a-zA-Z0-9[:space:]]//g' "$file" > "$DIR_years/$(basename "$file" .txt)_clean.txt"
+	       continue
+       fi
+       
+       filename=$(basename "$file" .txt)
+       [ $first_year -eq 0 ] && first_year=$filename
+       curr_year=$filename
+       
+       # The following command translates all characters to lowercase, and then removes extraneous characters. We preserve apostrophes, but only when they appear intraword. Since sed does not support negative lookahead or lookbehind, I simply converted acceptable apostrophes to tildes, then deleted all apostrophes, and switched tildes back to apostrophes.
+       
+       # preprocessing
+       tr 'A-Z' 'a-z' < "$file" | sed 's/[][,#+=:;\/\?\‑\–\—\‘\’\“\”\°\`\|\~\£\$\&\"\.\(\)\*\%\-]//g' | sed -E "s/([A-Za-z])'([A-Za-z])/\1~\2/g" | sed "s/'//g" | sed "s/~/'/g" > "$DIR_years/${filename}_clean.txt"
+
 	if [[ STOP -eq 1 ]]; then
 		break
 	fi
 done
 
+echo "[$(basename "$0")]: Successfully preprocessed transcripts for years ${first_year} through ${curr_year}."
 
-# echo "[$(basename "$0")]: No errors."
-
-
+# for testing purposes, move to relevant section and uncomment
+#tr 'A-Z' 'a-z' < "$HOME/project/resources/stop_char_tester" | sed "s/[][,#+=:;\/\?\‑\–\—\‘\’\“\”\°\`\|\~\£\$\&\"\.\(\)\*\%\-]//g" > "$HOME/project/.temp/stop_char_out"
